857098022625b580d942f83f83b82ae11c90bf81
tycen <tycen@tycen.com>
2020-10-20 15:10:46 -0700

    cleaning up script

--- hostname_ip_reboot.sh
+++ hostname_ip_reboot.sh
@@ -2,7 +2,10 @@
 
 /bin/hostname > /home/pi/hostname_ip.txt
 
-for i in eth0 wlan0 ; do echo $i >> /home/pi/hostname_ip.txt ; /sbin/ifconfig | grep -A 1 $i | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' >> /home/pi/hostname_ip.txt ; done
+for i in eth0 wlan0 ; do
+  echo $i >> /home/pi/hostname_ip.txt
+  /sbin/ifconfig | grep -A 1 $i | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' >> /home/pi/hostname_ip.txt
+done
 /bin/date >> /home/pi/hostname_ip.txt
 
 cat /home/pi/hostname_ip.txt | mail -s "I rebooted" your@email.com
